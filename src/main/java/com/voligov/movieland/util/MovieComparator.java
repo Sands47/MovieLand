2fe62ed2cb9010fe5e2e9700e98085ced14af873
Sands47 <sands47@gmail.com>
2016-06-10 18:23:05 +0300

    changed sorting implementation

--- src/main/java/com/voligov/movieland/util/MovieComparator.java
+++ src/main/java/com/voligov/movieland/util/MovieComparator.java
@@ -17,30 +17,14 @@ public class MovieComparator implements Comparator<Movie> {
     public int compare(Movie movie1, Movie movie2) {
         int result = 0;
         if ("asc".equals(ratingOrder)) {
-            if (movie1.getRating() > movie2.getRating()) {
-                result = 1;
-            } else if (movie1.getRating() < movie2.getRating()) {
-                result = -1;
-            }
+            result = movie1.getRating().compareTo(movie2.getRating());
         } else if ("desc".equals(ratingOrder)) {
-            if (movie1.getRating() < movie2.getRating()) {
-                result = 1;
-            } else if (movie1.getRating() > movie2.getRating()) {
-                result = -1;
-            }
+            result = -movie1.getRating().compareTo(movie2.getRating());
         }
         if (result == 0 && "asc".equals(priceOrder)) {
-            if (movie1.getPrice() > movie2.getPrice()) {
-                result = 1;
-            } else if (movie1.getPrice() < movie2.getPrice()) {
-                result = -1;
-            }
+            result = movie1.getPrice().compareTo(movie2.getPrice());
         } else if (result == 0 && "desc".equals(priceOrder)) {
-            if (movie1.getPrice() < movie2.getPrice()) {
-                result = 1;
-            } else if (movie1.getPrice() > movie2.getPrice()) {
-                result = -1;
-            }
+            result = -movie1.getPrice().compareTo(movie2.getPrice());
         }
         return result;
     }
